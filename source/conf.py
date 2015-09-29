0f6406fb643b6dfa440107f283ec73295ef32d3e
Carsten Thiel <thiel@sub.uni-goettingen.de>
2015-09-29 14:44:48 +0200

    bump version in response to license

--- source/conf.py
+++ source/conf.py
@@ -60,7 +60,7 @@ license_link = u'http://creativecommons.org/licenses/by/4.0/'
 # built documents.
 #
 # DEFINE FULL VERSION AND RELEASE
-version = '0.0.8'
+version = '0.1.0'
 get_cendari_release = os.environ.get('CENDARI_RELEASE', None)
 if get_cendari_release != None:
     release = os.environ.get('CENDARI_RELEASE')
